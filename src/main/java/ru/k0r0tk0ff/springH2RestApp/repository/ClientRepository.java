bf665b2f4994ca45e4dcbbe7a62de90d9077abd1
k0r0tk0ff <korotkov34reg@gmail.com>
2017-10-24 19:04:34 +0300

    rewrite, use JPA repo

--- src/main/java/ru/k0r0tk0ff/springH2RestApp/repository/ClientRepository.java
+++ src/main/java/ru/k0r0tk0ff/springH2RestApp/repository/ClientRepository.java
@@ -1,14 +1,10 @@
 package ru.k0r0tk0ff.springH2RestApp.repository;
 
-import java.util.Collection;
-
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import ru.k0r0tk0ff.springH2RestApp.model.Client;
 
 @Repository
-public interface ClientRepository extends CrudRepository<Client, Long> {
-	
-	Collection<Client> findAll();
-
+public interface ClientRepository extends JpaRepository<Client, Long> {
 }
--- src/main/java/ru/k0r0tk0ff/springH2RestApp/repository/PetRepository.java
+++ src/main/java/ru/k0r0tk0ff/springH2RestApp/repository/PetRepository.java
@@ -1,20 +1,11 @@
 package ru.k0r0tk0ff.springH2RestApp.repository;
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-import ru.k0r0tk0ff.springH2RestApp.model.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import ru.k0r0tk0ff.springH2RestApp.model.Pet;
 
-import java.util.Collection;
-
-/**
- * Created by user on 10/24/2017.
- */
-
 @Repository
-public interface PetRepository extends CrudRepository<Pet, Long> {
-
-    Collection<Pet> findAll();
-
+public interface PetRepository extends JpaRepository<Pet, Long> {
 }
 
