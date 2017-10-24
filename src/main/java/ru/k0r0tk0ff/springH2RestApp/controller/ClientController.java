5026d5118aa2c4d42cb413e0d983bf50d7fe1d1a
k0r0tk0ff <korotkov34reg@gmail.com>
2017-10-24 19:01:32 +0300

    add HTTP response "NOT FOUND"

--- src/main/java/ru/k0r0tk0ff/springH2RestApp/controller/ClientController.java
+++ src/main/java/ru/k0r0tk0ff/springH2RestApp/controller/ClientController.java
@@ -33,7 +33,7 @@ public class ClientController {
 		if (client != null) {
 			return new ResponseEntity<>(repo.findOne(id), HttpStatus.OK);
 		} else {
-			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 	}
 
